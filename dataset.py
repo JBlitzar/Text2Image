0a35b35ce2d59dabd5fccaa50cdf9d56353afef5
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-03-17 14:00:23 -0700

    dataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,10 @@
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
-cap = dset.CocoCaptions(root = 'data',
-                        annFile = 'data/annotations.json',
+cap = dset.CocoCaptions(root = 'data/train2017',
+                        annFile = 'data/annotations/captions_train2017.json',
                         transform=transforms.PILToTensor())
+print('Number of samples: ', len(cap))
+img, target = cap[3] # load 4th sample
+
+print("Image Size: ", img.size())
+print(target)
--- download_coco_dataset.sh
+++ download_coco_dataset.sh
@@ -1,7 +1,14 @@
-cd data
-curl -o train_imgs.zip http://images.cocodataset.org/zips/train2017.zip
-curl -o val_imgs.zip http://images.cocodataset.org/zips/val2017.zip
-curl -o test_imgs.zip http://images.cocodataset.org/zips/test2017.zip
+read -p "Continue? ~25gb download size (y/N) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+    cd data
+    curl -o train_imgs.zip http://images.cocodataset.org/zips/train2017.zip
+    curl -o val_imgs.zip http://images.cocodataset.org/zips/val2017.zip
+    curl -o test_imgs.zip http://images.cocodataset.org/zips/test2017.zip
 
 
-curl -o annotations_trainval.zip http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+    curl -o annotations_trainval.zip http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+fi
+
