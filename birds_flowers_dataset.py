27ba14a7a1f937db0367cd05004b8983966f9ff8
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-08-13 18:31:43 -0700

    birds flowers dataset

---
+++ birds_flowers_dataset.py
@@ -0,0 +1,115 @@
+from datasets import load_dataset
+import torch
+from torchvision.transforms import v2
+import os
+import glob
+from PIL import Image
+import tqdm
+# flowers = load_dataset("pranked03/flowers-blip-captions")
+# birds = load_dataset("anjunhu/naively_captioned_CUB2002011_test_20shot")
+#flowers.set_format(type="torch", columns=["text", "image"])
+#birds.set_format(type="torch", columns=["text", "image"])
+
+
+img_size = 64
+transforms = v2.Compose([
+    #v2.PILToTensor(),
+    v2.ToImage(), 
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Resize(img_size),
+    v2.CenterCrop(size=(img_size, img_size))
+    
+
+])
+
+class BirdsDataset(torch.utils.data.Dataset):
+    def __init__(self, hf_path: str, transform = transforms):
+        super().__init__()
+
+        self._interalds = load_dataset(hf_path)["train"]
+
+        self.transform = transform
+
+
+
+    def __len__(self):
+        return len(self._interalds)
+    
+    def __getitem__(self, idx: int):
+        item = self._interalds[idx]
+
+        text = item["description"].split("\n")
+        text = text[torch.randint(0, len(text), (1,)).item()]
+
+        image = item["image"]
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return text, image
+    
+
+class FlowersDataset(torch.utils.data.Dataset):
+    def __init__(self, root: str, transform=transforms):
+        super().__init__()
+
+        self.transform = transform
+
+        self.imgs_dir = os.path.join(root, "flowers-102/jpg")
+        self.ann_dir = os.path.join(root, "flowers-102/text_c10")
+        self.ann_glob = glob.glob(os.path.join(self.ann_dir, "class_*/image_*.txt"))
+
+        # Preload all annotations and images into memory
+        self.data = []
+        for ann_path in tqdm.tqdm(self.ann_glob, leave=False):
+            with open(ann_path, "r") as file:
+                annotation = file.read()
+
+            img_id = os.path.splitext(os.path.basename(ann_path))[0]
+            img_path = os.path.join(self.imgs_dir, f"{img_id}.jpg")
+            image = Image.open(img_path).convert("RGB")
+            
+            
+            
+            self.data.append((annotation, image))
+
+    def __len__(self):
+        return len(self.data)
+    
+
+    def __getitem__(self, idx):
+        annotation, image = self.data[idx]
+        annotation = annotation.split("\n")
+        annotation = annotation[torch.randint(0, len(annotation), (1,)).item()]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return annotation, image
+
+
+
+flowers = FlowersDataset(root=os.path.expanduser("~/torch_datasets/flowers-full"))
+
+
+
+birds = BirdsDataset("Multimodal-Fatima/CUB_train")
+
+
+
+if __name__ == "__main__":
+    print(len(flowers))
+    print(len(birds))
+    flower = flowers[200]
+    bird = birds[200]
+
+    def examine(sample):
+        t, i = sample
+        print(t)
+        print(i.shape)
+        print(torch.max(i))
+        print(torch.min(i))
+        print(torch.mean(i))
+
+    examine(flower)
+    examine(bird)
